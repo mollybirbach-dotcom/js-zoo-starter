1a89baa4f5797590e259287ce63fdb7dc74b2f4c
mollybirbach-dotcom <mollybirbach@students.berkeley.net>
2026-02-19 19:39:40 +0000

    Commiting

--- script.js
+++ script.js
@@ -1,9 +1,120 @@
-//starter code
+//python -m http.server
+
+/*function sleep(name) {
+  console.log(name + " sleeps for 8 hours")
+}*/
+
+/*function eat(name, food) {
+  console.log(name + " eats " + food);
+  if (food == favoriteFood) {
+    console.log("YUM!!! " + name + " wants more " + food);
+  } else  {
+    sleep(name);
+  }
+}
+*/
+
+class Animal {
+
+  constructor(name, favoriteFood) {
+    this.name = name;
+    this.favoriteFood = favoriteFood;
+  }
+
+  sleep() {
+    console.log(this.name + " sleeps for 8 hours")
+
+  }
+
+  eat(food) {
+    console.log(this.name + " eats " + food);
+    if (food == this.favoriteFood) {
+      console.log("YUM!!! " + this.name + " wants more " + food);
+    } else {
+      this.sleep();
+    }
+  }
+}
+
+class Tiger extends Animal {
+
+  constructor(name) {
+    super(name, "meat");
+  }
+}
+
+class Bear extends Animal {
+
+  constructor(name) {
+    super(name,"fish");
+  }
+
+  sleep() {
+    console.log(this.name + " hibernates for 4 months");
+  }
+
+}
+
+class Unicorn extends Animal {
+  constructor(name) {
+    super(name,"marshmallows");
+  }
+
+  sleep() {
+    console.log(this.name + " sleeps in a cloud.")
+  }
+}
+
+class Giraffe extends Animal {
+
+  constructor(name) {
+    super(name,"leaves");
+    }
+  eat(food) {
+    if (food !== this.favoriteFood) {
+      console.log("YUCK!!! " + this.name + " will not eat " + food);
+    } else {
+      super.eat('leaves');
+    }
+  }
+
+}
+
+class Bee extends Animal {
+  constructor(name) {
+    super(name,"pollen");
+  }
+
+  eat(food) {
+    if (food == this.favoriteFood) {
+      console.log("YUM!!! " + this.name + " wants more pollen")
+      this.sleep();
+    } else {
+      console.log("YUCK!!! " + this.name + " will not eat " + food);
+    }
+  }
+
+
+  sleep() {
+    console.log(this.name + " never sleeps")
+  }
+}
 
 function run() {
-    sleep("Tigger");
-  }
-  
-  function sleep(name) {
-    console.log(name + " sleeps for 8 hours")
-  }
+  var tigger = new Tiger("Tigger");
+  tigger.eat("meat");
+  tigger.eat("kibble");
+  var pooh = new Bear("Pooh");
+  pooh.eat("fish");
+  pooh.eat("meat");
+  var rarity = new Unicorn("Rarity");
+  rarity.eat("marshmallows")
+  var gemma = new Giraffe("Gemma");
+  gemma.eat("meat");
+  gemma.eat("leaves");
+  var stinger = new Bee("Stinger");
+  stinger.eat("ice cream");
+  stinger.eat("pollen");
+}
+run();
+//saving
